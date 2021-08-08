605a51f0b0bfbdec5d4a953cbcea00b61b884578
dth88 <gdath100500@gmail.com>
2021-08-08 07:48:31 +0300

    Rewriting with soft-http lib

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,15 @@
-use std::io::Error;
-use std::net::SocketAddr;
-use std::convert::Infallible;
-use hyper::{Body, Request, Response, Server};
-use hyper::service::{make_service_fn, service_fn};
+mod handler;
+mod server;
+mod router;
+use server::Server;
 
-
-#[tokio::main]
-async fn main() {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-    let service = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(soft_serve))
-    });
-
-    let server = Server::bind(&addr).serve(service);
-
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
+fn main() {
+    let server = Server::new("localhost:3000");
+    server.run();
 }
 
 
-async fn soft_serve(_req: Request<Body>)
--> Result<Response<Body>, Error> {
-    let static_ = hyper_staticfile::Static::new("static/");
-    let response_future = static_.serve(_req).await;
-    response_future
-}
+
+
+
+
